a9d32d7c0f3b94b4cdf4c5caedbd77d8aa0c6535
WillRed07 <reddpowerz@gmail.com>
2025-08-27 16:57:44 -0700

    d-lu

---
+++ src/main/java/frc/robot/subsystems/Mailbox.java
@@ -0,0 +1,31 @@
+//import MailboxConstants; needs to be made first bruh
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
+import com.revrobotics.spark.SparkFlex; //download stuff
+
+public class Mailbox extends SubsystemBase{
+    private final Timer 
+    private final SparkFlex
+    //digital input object: sensorForCoral
+}
+
+public Mailbox(){
+
+}
+
+public setSpeed(){
+
+}
+
+public brake(){
+
+}
+
+public detectorForCoral(){
+
+}
+
+public scorerCoral(){
+
+}
